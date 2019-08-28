aee6c02c32fa5a3a09720f6f3102af65e0fbb39f
jude-lafitteIII <judewrs@gmail.com>
2019-08-28 15:20:14 +0100

    setup py file

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+version = None
+
+with open("tinyconf/__init__.py", "r") as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.split(' ')[-1]
+
+setuptools.setup(
+    name="tinyconf",
+    version=version,
+    author="JellyWX",
+    author_email="judewrs@gmail.com",
+    description="A declarative config parsing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jellywx/tinyconf",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+)
